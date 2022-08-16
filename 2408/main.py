413dca17135ce02c1971dfe736602540a52d7172
Alexander Podstrechnyy <tankalxat34@gmail.com>
2022-08-16 10:14:44 +0300

    Исправляю решение

--- 2408/main.py
+++ 2408/main.py
@@ -20,14 +20,15 @@ def saveOutput(result_dict, output_filename="output.txt"):
 
     # Записываем результат в необходимом формате в файл
     with open(output_filename, "w", encoding="UTF-8") as file:
-        file.write(f"{v}\n{e}\n{resultFileContent}")
+        file.write(f"{v}\n{e}\n{resultFileContent}"[:-1])
+
 
 # Программа работает только если она запускается на прямую, не импортируется
 if __name__ == "__main__":
     # Читаем файл построчно
     with open("input.txt", "r", encoding="UTF-8") as file:
         FILECONTENT = file.readlines()
-    
+
     # Создаем список строк. У каждой строки слева и справа обрезаем пробелы
     COMBINATIONS = list(map(lambda e: e.strip(), FILECONTENT[1:]))
     # Сохраняем количество переговорок
@@ -37,26 +38,28 @@ if __name__ == "__main__":
     G = {}
 
     # Цикл для каждой строки
-    for j in range(T):
-        # Цикл для каждого символа в строке
-        for i in range(len(COMBINATIONS[j]) - 2):
-            # Создаем начальную и конечную вершины графа
-            start_point = COMBINATIONS[j][i:i+3]
-            end_point = COMBINATIONS[j][i+1:i+4]
+    if T >= 1 and T <= 40000:
+        for j in range(T):
+            # Цикл для каждого символа в строке
+            for i in range(len(COMBINATIONS[j]) - 2):
+                # Создаем начальную и конечную вершины графа
+                if len(COMBINATIONS[j]) >= 4 and len(COMBINATIONS[j]) <= 30:
+                    start_point = COMBINATIONS[j][i:i+3]
+                    end_point = COMBINATIONS[j][i+1:i+4]
 
-            # Если количество символов в вершинах равно 3
-            if len(start_point) == 3 and len(end_point) == 3:
-                # Пытаемся инкрементировать счетчик для конкретных вершин
-                try:
-                    G[start_point][end_point] += 1
-                except KeyError:
-                    # Если счетчика для конкретных вершин не существует
-                    try:
-                        # Пытаемся инициализировать его
-                        G[start_point][end_point] = 1
-                    except KeyError:
-                        # Если же в start_point нет такой конечной вершины, создаем ее и инициализируем в ней счетчик
-                        G[start_point] = {}
-                        G[start_point][end_point] = 1
+                    # Если количество символов в вершинах равно 3
+                    if len(start_point) == 3 and len(end_point) == 3 and start_point == start_point.lower() and end_point == end_point.lower():
+                        # Пытаемся инкрементировать счетчик для конкретных вершин
+                        try:
+                            G[start_point][end_point] += 1
+                        except KeyError:
+                            # Если счетчика для конкретных вершин не существует
+                            try:
+                                # Пытаемся инициализировать его
+                                G[start_point][end_point] = 1
+                            except KeyError:
+                                # Если же в start_point нет такой конечной вершины, создаем ее и инициализируем в ней счетчик
+                                G[start_point] = {}
+                                G[start_point][end_point] = 1
 
     saveOutput(G)
