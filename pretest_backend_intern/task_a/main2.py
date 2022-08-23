2e825247e039686059434e8a80616485d3a4a447
Alexander Podstrechnyy <tankalxat34@gmail.com>
2022-08-24 01:32:36 +0300

    тупик

--- pretest_backend_intern/task_a/main2.py
+++ pretest_backend_intern/task_a/main2.py
@@ -2,33 +2,27 @@ import os
 os.chdir("task_a")
 
 
-def setIntWithProverka(e, userInput):
+def setIntWithProverka(e):
     if 1 <= int(e) and int(e) <= 10**9:
         return int(e)
     else:
-        if userInput:
-            print(-1)
-        else:
-            open("output.txt", "w").write(str(-1))
+        print(-1)
+        open("output.txt", "w").write(str(-1))
         quit()
 
-userInput = True
 try:
     N = int(input())
-    V = list(map(lambda e, userInput=userInput: setIntWithProverka(e, userInput), input().split(" ")))
+    V = list(map(lambda e: setIntWithProverka(e), input().split(" ")))
 except ValueError:
-    userInput = False
     f = open("input.txt")
     content = f.readlines()
     N = int(content[0])
-    V = list(map(lambda e, userInput=userInput: setIntWithProverka(e, userInput), content[1].split(" ")))
+    V = list(map(lambda e: setIntWithProverka(e), content[1].split(" ")))
     f.close()
 
 if not (len(V) == N or not (1 <= N and N <= 100000)):
-    if userInput:
-        print(-1)
-    else:
-        open("output.txt", "w").write(str(-1))
+    print(-1)
+    open("output.txt", "w").write(str(-1))
     quit()
 
 mx_v = max(V)
@@ -46,14 +40,10 @@ while len(set(V)) != 1:
     k = 0
 
     if counter >= 1000:
-        if userInput:
-            print(-1)
-        else:
-            open("output.txt", "w").write(str(-1))
+        print(-1)
+        open("output.txt", "w").write(str(-1))
         quit()
 
 
-if userInput:
-        print(counter)
-else:
-    open("output.txt", "w").write(str(counter))
+print(counter)
+open("output.txt", "w").write(str(counter))
