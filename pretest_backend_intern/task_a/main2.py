db660bbdd0c2d52446924e9f8c96008d7033f2ce
Alexander Podstrechnyy <tankalxat34@gmail.com>
2022-08-24 10:31:23 +0300

    Взял предложенное решение
    https://ru.stackoverflow.com/questions/1442169/%d0%af%d0%bd%d0%b4%d0%b5%d0%ba%d1%81-%d0%9a%d0%be%d0%bd%d1%82%d0%b5%d1%81%d1%82-wrong-answer-%d0%bd%d0%b0-%d0%bf%d0%be%d1%81%d0%bb%d0%b5%d0%b4%d0%bd%d0%b5%d0%bc-%d1%82%d0%b5%d1%81%d1%82%d0%b5

--- pretest_backend_intern/task_a/main2.py
+++ pretest_backend_intern/task_a/main2.py
@@ -10,6 +10,7 @@ def setIntWithProverka(e):
         open("output.txt", "w").write(str(-1))
         quit()
 
+
 try:
     N = int(input())
     V = list(map(lambda e: setIntWithProverka(e), input().split(" ")))
@@ -25,25 +26,13 @@ if not (len(V) == N or not (1 <= N and N <= 100000)):
     open("output.txt", "w").write(str(-1))
     quit()
 
-mx_v = max(V)
-k = 0
-counter = 0
-while len(set(V)) != 1:
-    for i in range(len(V)):
-        if V[i] != mx_v:
-            k += 1
+cnt = 0
+for i in range(1, len(V)):
+    d = V[i] - V[i-1]
+    if d < 0:
+        cnt = -1
+    elif d > 0:
+        cnt += d
 
-    for k_value in range(k):
-        V[k_value] += 1
-    counter += 1
-
-    k = 0
-
-    if counter >= 1000:
-        print(-1)
-        open("output.txt", "w").write(str(-1))
-        quit()
-
-
-print(counter)
-open("output.txt", "w").write(str(counter))
+print(cnt)
+open("output.txt", "w").write(str(cnt))
