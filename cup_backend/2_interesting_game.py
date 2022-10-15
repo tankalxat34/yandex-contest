b9d2e1773de645963615659c30ddfa2747c37af1
Alexander Podstrechnyy <tankalxat34@gmail.com>
2022-10-15 23:55:28 +0300

    Потрясающие (нет) решения второй задачи

--- cup_backend/2_interesting_game.py
+++ cup_backend/2_interesting_game.py
@@ -3,10 +3,13 @@ os.chdir(os.getcwd() + "\\cup_backend")
 
 ### ### ### ###
 
+import sys
+
 GAMER1 = "Vasya"
 GAMER2 = "Petya"
+DRAW = "Draw"
 
-winner = ""
+winner = DRAW
 
 score = {
     GAMER1: 0,
@@ -19,26 +22,35 @@ with open("input.txt", "r", encoding="UTF-8") as file:
     CARDS = list(map(int, file.readline().split()))
 
 
+def writeAnswer(answer: str):
+    open("output.txt", "w", encoding="UTF-8").write(answer)
+    sys.exit(0)
+
 for card_int in CARDS:
     if card_int % 5 == 0:
         score[GAMER1] += 1
     elif card_int % 3 == 0:
         score[GAMER2] += 1
-    elif not(card_int % 3 == 0 and card_int % 5 == 0) or not(card_int % 3 == 1 and card_int % 5 == 1):
-        score["uncollected_sum"] += 1
-    
-# проверка на победителя
-if score[GAMER1] >= K:
-    winner = GAMER1
-elif score[GAMER2] >= K:
-    winner = GAMER2
-else:
+
+    if score[GAMER1] >= K:
+        winner = GAMER1
+        writeAnswer(winner)
+    elif score[GAMER2] >= K:
+        winner = GAMER2
+        writeAnswer(winner)
+
+
+if score[GAMER2] < K and score[GAMER1] < K:
     if score[GAMER1] > score[GAMER2]:
         winner = GAMER1
-    elif score[GAMER1] < score[GAMER2]:
+        writeAnswer(winner)
+    elif score[GAMER2] > score[GAMER1]:
         winner = GAMER2
+        writeAnswer(winner)
     else:
-        winner = ""
+        winner = DRAW
+        writeAnswer(winner)
+        
 
 with open("output.txt", "w", encoding="UTF-8") as file:
     file.write(winner)
